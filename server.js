9a514722f9395aaf2dd9e56243e3569587e0f93a
rf464 <rf464@live.mdx.ac.uk>
2021-01-29 01:30:20 +0000

    new commit

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.delete('/collection/:collectionName/:id', (req, res, next) => {
     
     
     //this tells the server to listen on port 3000
-    const port = process.env.PORT || 3000
+    const port = process.env.PORT || 30000
     app.listen(port, ()=> {
         console.log('localhost 3000 running');
     })
